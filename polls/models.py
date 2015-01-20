fd1a6ca08a261ec4e2174e934ab589c33b968e97
Chloe Donegan <chloedonegan@gmail.com>
2015-01-20 21:12:13 +0000

    Creating a polls website- first view done

---
+++ polls/models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published recently?"
+
+# question_text = field name
+# Each field is represented by an instance of Field class
+# - CharField for character field
+# Some Field classes have required arguments. CharField, for example, requires that you give it a max_length.
+# A Field can also have various optional arguments; in this case, we’ve set the default value of votes to 0.
+# Finally, note a relationship is defined, using ForeignKey. That tells Django each Choice is related to a single Question. Django supports all the common database relationships: many-to-one, many-to-many and one-to-one.
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
